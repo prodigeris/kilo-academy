b6616c0c50c0b2b26c8c6fb6b56f7cb63193531b
rositatisor <70884246+rositatisor@users.noreply.github.com>
2021-07-12 20:20:05 +0300

    HW-02 Create WorkoutPlanHandler class (could be also Repository)

---
+++ E02-SOLID/rosita/WorkoutPlanHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Services;
+
+
+class WorkoutPlanHandler
+{
+    private int $version;
+
+    public function __construct(int $version)
+    {
+        $this->version = $version;
+    }
+
+    public function getOneByVersionScoreAndCount(int $version, int $score, int $workoutCount): ?WorkoutPlan
+    {
+        return WorkoutPlan::where('training_plan->version', $this->version)
+            ->where('running_level', $score)
+            ->where('workout_count', $workoutCount)
+            ->first();
+    }
+
+    /**
+     * @return Collection|WorkoutPlan[]
+     */
+    public function getAllByVersionScoreAndCount(int $version, int $score, int $workoutCount): Collection
+    {
+        return WorkoutPlan::where('training_plan->version', $this->version)
+            ->where('running_level', $score)
+            ->where('workout_count', $workoutCount)
+            ->get();
+    }
+
+    public function getByVersionAndScore(int $version, int $score): ?WorkoutPlan
+    {
+        return WorkoutPlan::where('training_plan->version', $this->version)
+            ->where('running_level', $score)
+            ->first();
+    }
+
+    public function getByVersionAndCount(int $version, int $score): ?WorkoutPlan
+    {
+        return WorkoutPlan::where('training_plan->version', $this->version)
+            ->where('workout_count', $score)
+            ->first();
+    }
+}
