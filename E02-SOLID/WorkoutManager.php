4cbb43b0447a44a56e6da25de348a1a29289d2ce
benaskubilius <kubiliusb2003@gmail.com>
2021-07-12 20:32:21 +0300

    Refactoring getRandomWorkout()

--- E02-SOLID/WorkoutManager.php
+++ E02-SOLID/WorkoutManager.php
@@ -48,11 +48,16 @@ class WorkoutManager
 
     public function getRandomWorkout(): Workout
     {
-        $workout = Workout::inRandomOrder()->first();
+        $workout = $this->getRandomRecord();
         if (! $workout) {
             throw new RuntimeException('No workout has been found');
         }
         return $workout;
+    }
+
+    private function getRandomRecord(): Workout
+    {
+        return Workout::inRandomOrder()->first();
     }
 
     public function getRandomVisibleWorkout(): Workout
