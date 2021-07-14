372eadfa9cc4c6652a1fd792911aa7ae03344641
dovydasvg <dovydasvg@yahoo.fr>
2021-07-14 11:37:38 +0300

    Made getRandomWorkout to abstract function, because each workout has a different eloquent call.

--- E02-SOLID/Dovydas/AbstractWorkout.php
+++ E02-SOLID/Dovydas/AbstractWorkout.php
@@ -8,17 +8,10 @@ abstract class AbstractWorkout implements Randomizable
 {
     abstract public function getAllWorkouts(): array;
 
-    public function getRandomWorkout($visible = false): Workout
+    abstract public function getRandomWorkout($visible = false): Workout;
+
+    public function getRandom(): Workout
     {
-        if($visible){
-            $workout = Workout::where('is_visible', true)->inRandomOrder()->first();
-        }
-        else{
-            $workout = Workout::inRandomOrder()->first();
-        }
-        if (! $workout) {
-            throw new RuntimeException('No workout has been found');
-        }
-        return $workout;
+        return $this->getRandomWorkout();
     }
 }
